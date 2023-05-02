7bf318e6ab84f3260af2836322d49e75ea624e9d
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-05-02 19:21:23 +0600

    server create

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-const cors = require('cors');
 
-const chef = require('./data/chefData.json');
-const recipe = require('./data/recipeData.json');
 
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef server is running')
 });
 
-app.get('/chef', (req, res) => {
-    res.send(chef);
-})
-app.get('/recipe', (req, res) => {
-    res.send(recipe);
-});
 
-app.get('/chef/:id', (req, res) => {
-    const id = req.params.id;
-    const selectRecipe = chef.find(item => item.id === +id);
-    res.send(selectRecipe);
-})
-app.get('/recipe/:id', (req, res) => {
-    const id = req.params.id;
-    const selectRecipe = recipe.find(item => item.id === +id);
-    res.send(selectRecipe);
-})
-
-app.get('/recipe/categories/:id', (req, res) => {
-    const id = req.params.id;
-    const chefRecipeCategory = recipe.filter(item => item.recipeId === +id);
-    res.send(chefRecipeCategory);
-})
 
 
 app.listen(port, () => {
