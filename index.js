8ce567382c2dc78fb3c9fe8359ab239bfc817968
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-05-02 19:19:08 +0600

    server created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('chef server is running')
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
